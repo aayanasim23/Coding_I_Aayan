f28a0036902f762d709e4acf22b119f49a1a2415
Aayan Asim <aayan.asim23@gmail.com>
2024-09-25 14:56:09 -0400

    feat: Add user input functionality to Madlib.py

--- Labs/Madlib.py
+++ Labs/Madlib.py
@@ -1,1 +1,14 @@
+# One day, a (adjective) (animal) decided to visit the (place). Along the way, it found a (adjective) (noun) lying on the ground. Curious, the (animal) picked it up and showed it to a (occupation) passing by. "This must belong to the (adjective) (person's name)," said the (occupation). Together, they decided to take it to the (place) and find its rightful (noun).
 
+adjective1 = input ("Enter an adjective: ")
+animal1 = input ("Enter an animal: ")
+place1 = input ("Enter a place: ")
+adjective2 = input ("Enter an adjective: ")
+noun1 = input ("Enter a noun: ")
+animal2 = input ("Enter an animal: ")
+occupation1 = input ("Enter an occupation: ")
+adjective3 = input ("Enter an adjective: ")
+person1 = input ("Enter a person's name: ")
+occupation2 = input ("Enter an occupation: ")
+place2 = input ("Enter a place: ")
+noun2 = input ("Enter a noun: ")
