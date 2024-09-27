1fdd708088c2ecedf7a2bff7e6db41b1339c0c4c
Aayan Asim <aayan.asim23@gmail.com>
2024-09-27 11:24:54 -0400

    Finished

--- Labs/Madlib.py
+++ Labs/Madlib.py
@@ -5,7 +5,6 @@ animal1 = input ("Enter an animal: ")
 place1 = input ("Enter a place: ")
 adjective2 = input ("Enter an adjective: ")
 noun1 = input ("Enter a noun: ")
-animal2 = input ("Enter an animal: ")
 occupation1 = input ("Enter an occupation: ")
 adjective3 = input ("Enter an adjective: ")
 person1 = input ("Enter a person's name: ")
@@ -13,4 +12,4 @@ place2 = input ("Enter a place: ")
 noun2 = input ("Enter a noun: ")
 
 
-print("One day, a " + adjective1 + " "+ animal1 + " decided to visit the " + place1 + " Along the way, it found a " + adjective2 + " " + noun1 + " lying on the ground. Curious, the " + animal2 + " picked it up and showed it to a " + occupation1+ " passing by. 'This must belong to the " + adjective3 + " " + person1 + ", said the " +occupation1+ ". Together, they decided to take it to the " +place2 + " and find its rightful "+ noun2+".")
+print("One day, a " + adjective1 + " "+ animal1 + " decided to visit the " + place1 + " Along the way, it found a " + adjective2 + " " + noun1 + " lying on the ground. Curious, the " + animal1 + " picked it up and showed it to a " + occupation1+ " passing by. 'This must belong to the " + adjective3 + " " + person1 + ", said the " +occupation1+ ". Together, they decided to take it to the " +place2 + " and find its rightful "+ noun2+".")
