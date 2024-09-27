c85f0cb7426f7c2e071f4360f0c418a5ac0d227f
Aayan Asim <aayan.asim23@gmail.com>
2024-09-27 11:22:13 -0400

    Madlib Complete

--- Labs/Madlib.py
+++ Labs/Madlib.py
@@ -1,4 +1,4 @@
-# One day, a (adjective) (animal) decided to visit the (place). Along the way, it found a (adjective) (noun) lying on the ground. Curious, the (animal) picked it up and showed it to a (occupation) passing by. "This must belong to the (adjective) (person's name)," said the (occupation). Together, they decided to take it to the (place) and find its rightful (noun).
+# One day, a (adjective) (animal) decided to visit the (place). Along the way, it found a (adjective) (noun) lying on the ground. Curious, the (animal) picked it up and showed it to a (occupation) passing by. "This must belong to the (adjective) (person's name)," said the (person's name). Together, they decided to take it to the (place) and find its rightful (noun).
 
 adjective1 = input ("Enter an adjective: ")
 animal1 = input ("Enter an animal: ")
@@ -9,6 +9,8 @@ animal2 = input ("Enter an animal: ")
 occupation1 = input ("Enter an occupation: ")
 adjective3 = input ("Enter an adjective: ")
 person1 = input ("Enter a person's name: ")
-occupation2 = input ("Enter an occupation: ")
 place2 = input ("Enter a place: ")
 noun2 = input ("Enter a noun: ")
+
+
+print("One day, a " + adjective1 + " "+ animal1 + " decided to visit the " + place1 + " Along the way, it found a " + adjective2 + " " + noun1 + " lying on the ground. Curious, the " + animal2 + " picked it up and showed it to a " + occupation1+ " passing by. 'This must belong to the " + adjective3 + " " + person1 + ", said the " +occupation1+ ". Together, they decided to take it to the " +place2 + " and find its rightful "+ noun2+".")
