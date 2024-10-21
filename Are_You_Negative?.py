6f8def918e78c26898e7a66428e4c4d81330ee5a
Aayan Asim <aayan.asim23@gmail.com>
2024-10-21 14:46:46 -0400

    Elif

--- Are_You_Negative?.py
+++ Are_You_Negative?.py
@@ -1,3 +1,5 @@
 number = 13
 if number < 0:
     print("The number is negative!")
+else:
+    print("The number is positive!")
---
+++ Elif.py
@@ -0,0 +1,11 @@
+number = 13
+if number < 0:
+    print("The number is negative!")
+
+
+elif number > 0:
+    print("The number is positive!")
+
+
+elif number == 0:
+    print("The number is neither positive or negative!")
