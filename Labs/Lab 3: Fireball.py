478077a883eb7585759aaa9e96038ab325636825
Aayan Asim <aayan.asim23@gmail.com>
2024-10-29 14:52:55 -0400

    Fireball Lab

---
+++ Labs/Lab 3: Fireball.py
@@ -0,0 +1,7 @@
+#In the board game Dungeons and Dragons, players can use the powerful fireball spell to burn their enemies to a crisp. Players roll five 6 sided dice to determine how much damage they deal with the fireball.
+
+#Write a program using nested For Loops that prints every possible roll fireball can produce with five 6 sided dice. Assume that each dice is discreet. After the program has printed the values, print the total number of possible dice rolls.
+
+
+#Challenge 1: Allow the user to change the side number of all the dice with an input
+#Challenge 2: Allow the user to change the side number of each individual die with inputs
