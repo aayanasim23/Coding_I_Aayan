bb7d91e133c00eed77f3757ef46d039a16c21d8e
Aayan Asim <aayan.asim23@gmail.com>
2024-11-19 14:54:26 -0500

    Trianges!

---
+++ Exercise Triangles.py
@@ -0,0 +1,8 @@
+def triangle_area(base, height):
+    area = (base * height * 0.5)
+    print(area)
+    
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+triangle_area(base, height)
