9fe27b09ecf5fe0d6aef5d9c0b2966d650f93ef3
Aayan Asim <aayan.asim23@gmail.com>
2024-11-19 10:36:36 -0500

    Work

---
+++ Exercise: Sum of Two Numbers.py
@@ -0,0 +1,6 @@
+def two_num_sum():
+    num1 = input("Give me a number")
+    num2 = input("Give me another number")
+    print(int(num1) + int(num2))
+
+two_num_sum()
--- Labs/HAL.py
+++ Labs/HAL.py
@@ -3,3 +3,14 @@ print("My name is HAL-9, I am your personal Chatbot.")
 name = input("What's your name?")
 
 print("Nice to meet you, " + name)
+
+
+while True:
+    
+    user_command = input("Tell me to do something," + name + ": ")
+
+    if user_command == "Print your name":
+        print("HAL-9")
+        break
+    else:
+        print("I’m sorry " + name + ", I’m afraid I can’t do that.")
