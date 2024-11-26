992ad9c6881c7f07ae66fed5684e8bf98cb40ca0
Aayan Asim <aayan.asim23@gmail.com>
2024-11-26 14:51:56 -0500

    fixed lab

--- Labs/mathfunctions.py
+++ Labs/mathfunctions.py
@@ -3,8 +3,7 @@ def isEven (number):
     if (number % 2) == 0:
         return(True)
     else:
-        return(False)
-isEven(2)
+
 
 def minVal(numb1, numb2):
     if numb1 < numb2:
@@ -12,7 +11,6 @@ def minVal(numb1, numb2):
     else:
         return(numb2)
 
-minVal(3, 2)
 
 
 print(isEven(minVal), minVal(2,3))
