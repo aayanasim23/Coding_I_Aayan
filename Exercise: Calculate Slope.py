d59b9b364c7cdcd0146fe81551abcb2b0903cb6d
Aayan Asim <aayan.asim23@gmail.com>
2025-02-12 15:58:22 -0500

    yes

--- Exercise: Calculate Slope.py
+++ Exercise: Calculate Slope.py
@@ -1,1 +1,7 @@
-def slope()
+def slope(p1, p2):
+    return (p2[1] - p1[1]) / (p2[0] - p1[0]) #AI to help with formula!
+
+point1 = (1, 2)
+point2 = (3, 4)
+result = slope(point1, point2)
+print(result)
---
+++ Exercise: Moving Rankings.py
@@ -0,0 +1,8 @@
+movie_list = ['Big Hero 6', 'Coco', 'Despicable Me', 'The Lion King', 'Finding Nemo', '']
+movie_list[0] = 'The Incredibles'
+movie_list[1] = 'Big Hero 6'
+movie_list[2] = 'Coco'
+movie_list[3] = 'Despicable Me'
+movie_list[4] = 'The Lion King'
+movie_list[5] = 'Finding Nemo'
+print(movie_list)
