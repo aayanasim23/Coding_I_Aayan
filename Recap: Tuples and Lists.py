4cfdfa6c3b6091aba0ead8711a409b6a5c8c1b98
Aayan Asim <aayan.asim23@gmail.com>
2025-02-24 14:54:53 -0500

    c

---
+++ Recap: Tuples and Lists.py
@@ -0,0 +1,8 @@
+Feb_break = ("Friday, Relaxed from school", "Saturday, Played Flight Simulator", "Sunday, Played Flight Simulator", "Monday, Played Flight Simulator", "Tuesday, Played Flight Simulator", "Wednesday, Played Flight Simulator", "Thursday, Went to JFK to spot planes", "Friday, Went to New Jersey", "Saturday, Went outside with my cousins", "Sunday, Practiced flying at flight school")
+print(Feb_break)
+
+Feb_break[1]
+weekends = Feb_break[:3] + Feb_break[8:10]
+print(weekends)
+
+#NOT FINISHED YET
