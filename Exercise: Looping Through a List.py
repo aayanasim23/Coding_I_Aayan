42d316a92a1081be5a098fb9555089f47a85c89f
Aayan Asim <aayan.asim23@gmail.com>
2025-02-28 22:40:00 -0500

    t

---
+++ Exercise: Looping Through a List.py
@@ -0,0 +1,5 @@
+list =[5,3,8,11,1,-4,2]
+sum = 0
+for number in list:
+    sum = sum+number
+print(sum)
---
+++ Exercise: Max Value.py
@@ -0,0 +1,6 @@
+list = [5,20,503,229,29,67]
+sum = 0
+for num in list:
+    sum = sum + num
+
+print(sum)
