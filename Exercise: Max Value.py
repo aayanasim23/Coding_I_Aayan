9332227aa078d936efee19220a1623ec6ad0d42f
Aayan Asim <aayan.asim23@gmail.com>
2025-03-04 21:51:38 -0500

    challenge

--- Exercise: Max Value.py
+++ Exercise: Max Value.py
@@ -1,6 +1,15 @@
-list = [5,20,503,229,29,67]
+list = [5,804,503,229,29,67]
 sum = 0
+minimum = list[0] # (CHALLENGE) After failure of troubleshooting, I searched this part up. In able for this to work, we have to compare the values to the list. Originally I put minimum = 0 which would constantly give me 0 as the minimum.
 for num in list:
-    sum = sum + num
+    if num > sum:
+        sum = num
+
+for nums in list:
+    if nums < minimum:
+        minimum = nums
+
 
 print(sum)
+print(minimum)
+
