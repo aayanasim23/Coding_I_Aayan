dbcade72a75691fb0107cff22b4fe7f63a5ba593
Aayan Asim <aayan.asim23@gmail.com>
2025-03-05 14:55:42 -0500

    t

--- Labs/Guess The Word part 1 .py
+++ Labs/Guess The Word part 1 .py
@@ -2,5 +2,12 @@ secret_word = "skull"
 
 def get_guess():
     guess = input("Guess the word")
-    if guess .isupper
-        print("bro")
+    while not guess.islower() and len(guess) == 1:
+        print("Try Again!")
+        guess = input("Guess the word")
+    if guess = 
+
+
+get_guess()
+
+# PART 1 ALMOST FINISHED
