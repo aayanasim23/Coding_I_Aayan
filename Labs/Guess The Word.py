e82f17036c3e63d1f5011d479a2443d65a0a1a1a
Aayan Asim <aayan.asim23@gmail.com>
2025-03-18 20:47:11 -0400

    m

--- Labs/Guess The Word.py
+++ Labs/Guess The Word.py
@@ -6,22 +6,26 @@ def get_guess():
     while not (len(guess) == 1 and guess.islower()):
         print("Try Again!")
         guess = input("Guess a letter: ")
+
     if guess in secret_word:
         return guess
 
 print("You guessed:", get_guess())
 
+
 def play_game():
     attempts = 10
+
     while attempts > 0:
-        guess = get_guess()
+        guess = get_guess() 
 
-        if guess == secret_word:
-            print("Congrats! You guessed the word!")
-            return
+        if guess in secret_word:  
+            print("Correct guess!")
+        else:
+            print("Wrong guess.")
 
-        print("Wrong guess. Attempts left:" , attempts - 1)
         attempts -= 1
+        print("Attempts left:", attempts)
 
     print("Game Over! The secret word was:", secret_word)
 
