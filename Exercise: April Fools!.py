c9c31d056ad6e3509ecbf030b25b3114a3106a7b
Aayan Asim <aayan.asim23@gmail.com>
2025-04-01 23:49:19 -0400

    almost finished

---
+++ Exercise: April Fools!.py
@@ -0,0 +1,15 @@
+april = {
+"Sunday" : False,
+"Monday" : False,
+"Tuesday" : True,
+"Wednesday" : False,
+"Thursday" : False,
+"Friday" : False,
+"Saturday" : False
+}
+
+
+for fools in april:
+
+
+#almost done
